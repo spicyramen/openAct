e04a7f27c3a9842b7ba2349e1746c9b56f32e578
root <you@example.com>
2015-03-28 12:43:48 -0400

    Add mobicents

--- mobicents/installMobicents.sh
+++ mobicents/installMobicents.sh
@@ -6,13 +6,32 @@
 trap "rm .f 2> /dev/null; exit" 0 1 3
 # Initialize our own variables:
 MOBICENTS_URL="https://github.com/Mobicents/sip-servlets/releases/download/v3.0.564/mss-3.0.564-jboss-as-7.2.0.Final.zip"
+MOBICENTS_FILE="mss-3.0.564-jboss-as-7.2.0.Final.zip"
 MOBICENTS_DIR="mss-3.0.564-jboss-as-7.2.0.Final"
+IP_ADDR=`ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}'`
+
 #Update system
 apt-get install zip -y
+
+function check {
+	ERROR_CODE=$(echo "$?")
+    	 if [ $ERROR_CODE -ne 0 ]; then
+	       	echo "Error ($ERROR_CODE) Fatal Exception occurred"
+	        exit 1
+    	 fi
+     	return 0
+}
 
 # Get Mobicents
 cd /usr/local/src
 wget $MOBICENTS_URL
-unzip $MOBICENTS_URL
-cd $MOBICENTS_DIR
-
+check;
+unzip $MOBICENTS_FILE
+rm -rf $MOBICENTS_FILE
+sed -r 's/(enable-welcome-root=")[^"]+"/\1false"/' $MOBICENTS_DIR/standalone/configuration/standalone-sip.xml
+cp $MOBICENTS_DIR/standalone/configuration/standalone-sip.xml $MOBICENTS_DIR/bin
+cd $MOBICENTS_DIR/bin
+echo "Kill any existing instance"
+./jboss-cli.sh --connect command=:shutdown
+echo "Start Mobicents"
+./standalone.sh -b $IP_ADDR -c standalone-sip.xml &
