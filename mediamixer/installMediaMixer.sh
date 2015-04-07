5ddce6a1215983896bda904c23ef77a2ddec0e29
Gonzalo Gasca Meza <gascagonzalo@gmail.com>
2015-04-06 23:11:14 -0400

    Add Java check

--- mediamixer/installMediaMixer.sh
+++ mediamixer/installMediaMixer.sh
@@ -11,7 +11,7 @@ CERT_COUNTRY="US"
 CERT_STATE="California"
 CERT_LOCATION="Milpitas"
 CERT_ORGANIZATION="Engineering"
-INSTALL_JAVA=0
+INSTALL_JAVA=1
 # This line is in Make file, we will replace it
 ORIG_CERT="@openssl req -nodes -new -x509 -keyout \$(BIN)/mcu.key -out \$(BIN)/mcu.crt"
 NEW_CERT="@openssl req -nodes -days $CERT_DURATION -new -x509 -keyout \$(BIN)/mcu.key -out \$(BIN)/mcu.crt -subj \"/C=$CERT_COUNTRY/ST=$CERT_STATE/L=$CERT_LOCATION/O=$CERT_ORGANIZATION/CN=$HOSTNAME\""
@@ -24,6 +24,70 @@ function check {
 	        exit 1
     	 fi
      	return 0
+}
+
+function check_java {
+
+REQUIRED_VERSION=1.6
+
+# Transform the required version string into a number that can be used in comparisons
+REQUIRED_VERSION=`echo $REQUIRED_VERSION | sed -e 's;\.;0;g'`
+# Check JAVA_HOME directory to see if Java version is adequate
+if [ $JAVA_HOME ]
+then
+	JAVA_EXE=$JAVA_HOME/bin/java
+	$JAVA_EXE -version 2> tmp.ver
+	VERSION=`cat tmp.ver | grep "java version" | awk '{ print substr($3, 2, length($3)-2); }'`
+	rm tmp.ver
+	VERSION=`echo $VERSION | awk '{ print substr($1, 1, 3); }' | sed -e 's;\.;0;g'`
+	if [ $VERSION ]
+	then
+		if [ $VERSION -ge $REQUIRED_VERSION ]
+		then
+			JAVA_HOME=`echo $JAVA_EXE | awk '{ print substr($1, 1, length($1)-9); }'`
+		else
+			JAVA_HOME=
+		fi
+	else
+		JAVA_HOME=
+	fi
+fi
+
+# If the existing JAVA_HOME directory is adequate, then leave it alone
+# otherwise, use 'locate' to search for other possible java candidates and
+# check their versions.
+if [ $JAVA_HOME ]
+then
+	:
+else
+	for JAVA_EXE in `locate bin/java | grep java$ | xargs echo`
+	do
+		if [ $JAVA_HOME ] 
+		then
+			:
+		else
+			$JAVA_EXE -version 2> tmp.ver 1> /dev/null
+			VERSION=`cat tmp.ver | grep "java version" | awk '{ print substr($3, 2, length($3)-2); }'`
+			rm tmp.ver
+			VERSION=`echo $VERSION | awk '{ print substr($1, 1, 3); }' | sed -e 's;\.;0;g'`
+			if [ $VERSION ]
+			then
+				if [ $VERSION -ge $REQUIRED_VERSION ]
+				then
+					JAVA_HOME=`echo $JAVA_EXE | awk '{ print substr($1, 1, length($1)-9); }'`
+				fi
+			fi
+		fi
+	done
+fi
+
+# If the correct Java version is detected, then export the JAVA_HOME environment variable
+if [ $JAVA_HOME ]
+then
+	export JAVA_HOME
+	echo $JAVA_HOME
+fi
+
 }
 
 #Update system
@@ -114,8 +178,7 @@ if [ $INSTALL_JAVA -eq 1 ]; then
 	echo $JAVA_HOME
 	export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 else
-	echo "Verify Java is already installed"
-	java -version
+	check_java;
 fi
 
 
